c2eeb044e5a2b289e5e353b3645236c6e61b8523
Muneeb2721 <muneebnadeem1870@gmail.com>
2026-02-14 01:01:52 +0500

    feat: add Contact page with Banner and ShoppingCart components

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+import Banner from "@/components/Banner/banner";
+import ShoppingCart from "@/components/ui/cart";
+
+const Contact = () => {
+    return (
+        <>
+            <Banner type="contact" />
+            <ShoppingCart />
+        </>
+    ) 
+}
+
+export default Contact;
